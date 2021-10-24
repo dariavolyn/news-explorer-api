8884682de0b1000f38e3e20eea07d905032e672e
Dasha V <dariavolyn@yandex.ru>
2021-10-24 16:27:08 -0400

    cors

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ app.use(requestLogger);
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
+  }
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
