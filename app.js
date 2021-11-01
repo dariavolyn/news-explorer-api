68f356b1f4ccd36510480334c76127c725bf2fff
dariavolyn <75863158+dariavolyn@users.noreply.github.com>
2021-11-01 17:39:22 -0400

    auth

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ const allowedCors = [
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
