94e5b79b268bc5236852ef6483f7cdefc248007e
dariavolyn <75863158+dariavolyn@users.noreply.github.com>
2021-11-01 16:32:38 -0400

    auth

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-const auth = require('./middlewares/auth');
+// const auth = require('./middlewares/auth');
 const articlesRouter = require('./routes/articles');
 const userRouter = require('./routes/user');
 const { createUser, login } = require('./controllers/users');
@@ -41,7 +41,7 @@ app.post('/signin', celebrate({
   }),
 }), login);
 
-app.use(auth);
+// app.use(auth);
 
 app.use('/articles', articlesRouter);
 app.use('/users', userRouter);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,27 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+// const { NODE_ENV, JWT_SECRET } = process.env;
+// // eslint-disable-next-line consistent-return
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    const err = new Error('Authorization required b');
-    err.statusCode = 401;
-    next(err);
-  }
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     const err = new Error('Authorization required b');
+//     err.statusCode = 401;
+//     next(err);
+//   }
 
-  const token = authorization.replace('Bearer ', '');
-  let payload;
+//   const token = authorization.replace('Bearer ', '');
+//   let payload;
 
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-  } catch (e) {
-    const err = new Error('Authorization required p');
-    err.statusCode = 401;
-    next(err);
-  }
-  req.user = payload;
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+//   } catch (e) {
+//     const err = new Error('Authorization required p');
+//     err.statusCode = 401;
+//     next(err);
+//   }
+//   req.user = payload;
 
-  next();
-};
+//   next();
+// };
