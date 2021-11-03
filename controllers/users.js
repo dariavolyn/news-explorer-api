04978cc810efc6793da020e4b270d3438065a716
dariavolyn <75863158+dariavolyn@users.noreply.github.com>
2021-11-03 10:19:53 -0400

    auth

--- controllers/users.js
+++ controllers/users.js
@@ -62,7 +62,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' });
-      res.send({ token, user });
+      res.send({ token, user: user.username });
     })
     .catch(() => {
       const err = new Error('Invalid email or password');
